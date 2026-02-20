a37c89586b90818eaaba5227a2dce7e80c0127f0
MarcoAAguil4r <aguilar9122@gmail.com>
2026-02-19 19:58:44 -0600

    Crea y postea vacantes

--- apps/backend/src/routes/index.js
+++ apps/backend/src/routes/index.js
@@ -1,8 +1,6 @@
 const router = require("express").Router();
+
 router.use("/auth", require("./auth.routes"));
-<<<<<<< HEAD
 router.use("/jobs", require("./jobs.routes"));
+
 module.exports = router;
-=======
-module.exports = router;
->>>>>>> 23b8af0 (feat(auth): register y login con JWT)
